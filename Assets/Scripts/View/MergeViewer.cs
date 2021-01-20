01ece8ecf251128bbc94012904cf828183e299d6
Joel Ring <joel.ring@gmail.com>
2021-01-20 17:27:39 +0100

    adds merge runes to empty merge slots

--- Assets/Scripts/View/MergeViewer.cs
+++ Assets/Scripts/View/MergeViewer.cs
@@ -1,5 +1,6 @@
 using Model;
 using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEngine.UI;
 
 public class MergeViewer : MonoBehaviour{
@@ -7,13 +8,20 @@ public class MergeViewer : MonoBehaviour{
     private MergeInventory _mergeInventory;
     [SerializeField] private Transform[] mergeArray;
     // Start is called before the first frame update
+    private Color defaultColor;
     void Start(){
         _mergeInventory = FindObjectOfType<MergeInventory>();
         _mergeInventory.MergeRuneAdded += ShowMergeRune;
+        defaultColor = mergeArray[0].GetComponent<Image>().color;
     }
 
     private void ShowMergeRune(object sender, GameObject e){
-        mergeArray[0].GetComponent<Image>().overrideSprite = e.GetComponent<Image>().sprite;
-        mergeArray[0].GetComponent<Image>().color = e.GetComponent<Image>().color;
+        foreach (var slot in mergeArray){
+            if (slot.GetComponent<Image>().color != defaultColor)
+                continue;
+            slot.GetComponent<Image>().overrideSprite = e.GetComponent<Image>().sprite;
+            slot.GetComponent<Image>().color = e.GetComponent<Image>().color;
+            break;
+        }
     }
 }
