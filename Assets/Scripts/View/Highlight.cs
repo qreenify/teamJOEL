5818a26ebb7a93d196d128e93a99d3e63ea9178d
Joel Ring <joel.ring@gmail.com>
2021-01-21 09:03:13 +0100

    adds to namespace view

--- Assets/Scripts/View/Highlight.cs
+++ Assets/Scripts/View/Highlight.cs
@@ -1,42 +1,43 @@
-using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Highlight : MonoBehaviour, IPointerEnterHandler
-{
-    public Color DefaultColor{ get; set; }
-    public bool HighlightColor{ get; set; }
-    private Image Image => GetComponent<Image>();
-
-    private void Start(){
-        DefaultColor = Image.color;
-    }
-
-    //-> Remove highlight when hovered
-    public void OnPointerEnter(PointerEventData eventData)
+namespace View{
+    public class Highlight : MonoBehaviour, IPointerEnterHandler
     {
-        Image.color = DefaultColor;
-        HighlightColor = false;
-    }
+        public Color DefaultColor{ get; set; }
+        public bool HighlightColor{ get; set; }
+        private Image Image => GetComponent<Image>();
+
+        private void Start(){
+            DefaultColor = Image.color;
+        }
+
+        //-> Remove highlight when hovered
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Image.color = DefaultColor;
+            HighlightColor = false;
+        }
 
     
-    //-> Highlight newly added runes
-    public void EnableHighlight()
-    {
-        if (!HighlightColor){
-            Image.color += new Color(0.5f, 0.5f, 0.5f);
-            HighlightColor = true;
-        }
-            
-    }
-
-    private void Update()
-    {
-        //For testing
-        if (Input.GetKey(KeyCode.H))
+        //-> Highlight newly added runes
+        public void EnableHighlight()
         {
-            EnableHighlight();
+            if (!HighlightColor){
+                Image.color += new Color(0.5f, 0.5f, 0.5f);
+                HighlightColor = true;
+            }
+            
+        }
+
+        private void Update()
+        {
+            //For testing
+            if (Input.GetKey(KeyCode.H))
+            {
+                EnableHighlight();
+            }
         }
     }
 }
